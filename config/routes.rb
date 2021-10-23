21160c7b6dc3f93fd566b2a1d0e348808a1c3a36
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-23 10:14:14 +0900

    feat: userの新規作成・編集アクションの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
   namespace :admin do
     get 'users/index'
   end
@@ -7,5 +9,6 @@ Rails.application.routes.draw do
 		passwords: 'admin_users/passwords',
 	}
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  root 'admin/users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
