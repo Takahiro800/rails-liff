bdb067309c787a757c16e400862ea0a955e3e9b6
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-23 10:11:00 +0900

    feat: user一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'users/index'
   end
-  devise_for :admin_users
+  devise_for :admin_users, controllers: {
+		sessions: 'admin_users/sessions',
+		passwords: 'admin_users/passwords',
+	}
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
