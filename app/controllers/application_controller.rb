4e784ea0b3748e3cea9ed6829f03860f1fd0789d
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-24 15:23:50 +0900

    feat: lIFFのIDを環境変数として保存・活用するように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :setting_env_js
+
+
+  private
+  # LINEIDを環境変数としてJSに渡すためのもの
+  def setting_env_js
+    gon.LIFF_ID_key = ENV['LIFF_ID']
+  end
 end
