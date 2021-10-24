243fc966cb5f96bf67163ca660377de823aa8bc7
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-24 18:25:42 +0900

    feat: ユーザーの作成を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,16 @@ class UsersController < ApplicationController
     idToken = params[:idToken]
     channelId = ENV['CHANNEL_ID']
     res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), {'id_token'=>idToken, 'client_id'=>channelId})
-    render :json => res.body
+    line_user_id = JSON.parse(res.body)["sub"]
+    user = User.find_by(line_user_id: line_user_id)
+    if user.nil?
+      user = User.create(line_user_id: line_user_id)
+      session[:user_id] = user.id
+      render :json => user
+    elseif user
+      session[:user_id] = user.id
+      render :json => user
+    end
   end
 
   def edit
