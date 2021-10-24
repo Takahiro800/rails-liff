ad932a11ae7f6923c0a4b1c661587744672a2181
Takahiro Abe <hirotaka081630@gmail.com>
2021-10-24 17:40:47 +0900

    fix: actionMailerのコメントアウト

--- app/javascript/packs/liff.js
+++ app/javascript/packs/liff.js
@@ -11,7 +11,6 @@ window.onload = function () {
       if (!liff.isLoggedIn()) {
         liff.login();
       }
-      console.log('liff');
     })
     .catch((err) => {
       console.log(err.code, err.message);
