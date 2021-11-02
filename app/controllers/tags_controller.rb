08e4157a0421a528ab957a06d40e800a162600b8
Takahiro Abe <hirotaka081630@gmail.com>
2021-11-03 08:00:59 +0900

    refactor: typeの修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,7 @@ class TagsController < ApplicationController
     current_user = User.find(session[:user_id])
     @tag = current_user.tags.new(tag_params)
     if @tag.save
-      text = "新しくtタグ#{tag_params[:name]}を追加しました"
+      text = "新しくタグ#{tag_params[:name]}を追加しました"
 
       message = {
         type: 'text',
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,4 @@
 class User < ApplicationRecord
   has_many :tags, dependent: :destroy
 
-  def push_bot(user_line_id)
-    client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-
-    message={
-        type: 'text',
-        text: "hello"
-        }
-    user_id =  user_line_id
-    response = client.push_message(user_id, message)
-  end
 end
